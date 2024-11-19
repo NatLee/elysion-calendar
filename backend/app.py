ac2a1181ba4181b483ed4d7da1fe2d860b79d743
Nat Lee <natlee.work@gmail.com>
2024-11-19 15:56:48 +0800

    [update] CORS

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:3000").split(","),
+    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
